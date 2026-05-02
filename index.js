45803055178f84e53f6c04628dcecc9ba8ae2090
Juliana <juliana.ndeus@gmail.com>
2026-05-02 14:04:54 -0300

    recomecei :)

--- index.js
+++ index.js
@@ -1,54 +1,46 @@
-function addLista(){
-    let nome = document.getElementById("nomeLista").value;
-    let items = document.getElementById("inputItem").value;
+function verListas(){
+    const listas = JSON.parse(localStorage.getItem('listas')) || [];
+    const conteiner = document.getElementById('minhasListas');
+    if(listas == null || listas.length == 0){
+        const aviso = document.createElement('p');
+        aviso.id = 'aviso';
+        aviso.textContent = "Você ainda não criou nenhuma lista";
+        conteiner.appendChild(aviso);
+        return;
+    }
+    for(let i = 0; i < listas.length; i++){
+        const item = document.createElement('div');
+        const nome = document.createElement('p');
+        const verLista = document.createElement('button');
+        const excluirLista = document.createElement('button');
+        verLista.textContent = "Ver lista";
+        excluirLista.textContent = "Excluir"
+        nome.textContent = listas[i].nome;
+        item.appendChild(nome);
+        item.appendChild(verLista);
+        item.appendChild(excluirLista);
+        conteiner.appendChild(item);
+    }
+}
 
-    let obj = {
-        nome: nome,
+function addLista(){
+    const listas = JSON.parse(localStorage.getItem('listas')) || [];
+    const nomeDaLista = document.getElementById('nomeLista').value;
+    let itens;
+    if(nomeDaLista == null || nomeDaLista.length == 0){
+        window.alert("Você precisa dar um nome à lista");
+        return;
+    }
+    const obj = {
+        nome: nomeDaLista,
         itens: []
     }
-
-    let listas = JSON.parse(localStorage.getItem("listas")) || [];
-
     listas.push(obj);
-
     localStorage.setItem("listas", JSON.stringify(listas));
 }
 
-function mostrarListas(){ 
-    const container = document.getElementById("minhasListas"); 
-
-    const aviso = document.createElement("p"); 
-
-    aviso.textContent = "Você ainda não tem listas criadas"; 
-
-    let listas = JSON.parse(localStorage.getItem("listas")); 
-
-    if(listas == null){ 
-        const aviso = document.createElement("p"); 
-
-        aviso.textContent = "Você ainda não tem listas criadas";
-
-        container.appendChild(aviso); 
-    }else{
-        for(let i = 0; i < listas.length; i++){
-            const containerLista = document.createElement("ul");
-            const itemLista = document.createElement("li");
-            const nomeLista = document.createElement("h2");
-            nomeLista.textContent = listas[i].nome;
-            container.appendChild(containerLista);
-            containerLista.appendChild(itemLista);
-            itemLista.appendChild(nomeLista);
-        }
-    }
-}
-let x = 0;
 function inputItem(){
-    let container = document.getElementById("inputItem");
-    const input = document.createElement("input");
-    input.setAttribute("type", "text");
-    input.setAttribute("id", "item" + x);
-    container.appendChild(input);
-    x++;
+
 }
 
-mostrarListas();
+verListas();
