2f2193730b081ebc650d9a3b6d1beffd91ad4ad2
Juliana <juliana.ndeus@gmail.com>
2026-05-02 19:11:04 -0300

    funções pra adicionar itens a lista

--- lista.js
+++ lista.js
@@ -1,9 +1,22 @@
-function verLista(){
-    const listas = JSON.parse(localStorage.getItem("listas")) || [];
-    const index = localStorage.getItem("listaSelecionada");
+const listas = JSON.parse(localStorage.getItem("listas")) || [];
+const index = localStorage.getItem("listaSelecionada");
 
+function verLista(){
     const nome = document.getElementById('nomeDaLista');
+    const conteiner = document.getElementById('lista')
     nome.innerHTML = listas[index].nome;
+    for(let i = 0; i < listas[index].itens.length; i++){
+        const item = document.createElement('p');
+        item.textContent = listas[index].itens[i];
+        conteiner.appendChild(item);
+    }
+}
+
+function addItem(){
+    const item = document.getElementById('novoItem').value;
+    listas[index].itens.push(item);
+    localStorage.setItem("listas", JSON.stringify(listas));
+    window.location.reload();
 }
 
 verLista();
